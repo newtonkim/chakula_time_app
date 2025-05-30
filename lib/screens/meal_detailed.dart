8bcbc75fef824da266b64fca9701707037c42ca2
newtonkim <knewts98@gmail.com>
2025-05-30 10:01:56 +0300

    improving the meals detailed screen

--- lib/screens/meal_detailed.dart
+++ lib/screens/meal_detailed.dart
@@ -17,12 +17,54 @@ class MealDetailedScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover
+            ),
+            const SizedBox(height: 14),
+            Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              // ignore: deprecated_member_use
+              color: Theme.of(context).colorScheme.primary,
+            ),),
+            const SizedBox(height: 14),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient, 
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  // ignore: deprecated_member_use
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+            ),
+             const SizedBox(height: 24),
+            Text('Steps', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              // ignore: deprecated_member_use
+              color: Theme.of(context).colorScheme.primary,
+            ),),
+            const SizedBox(height: 14),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12, 
+                  vertical: 8
+                ),
+                child: Text(
+                  step, 
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    // ignore: deprecated_member_use
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                ),
+              ),
+          ]
+        ),
       ),
+
     );
   }
 }
